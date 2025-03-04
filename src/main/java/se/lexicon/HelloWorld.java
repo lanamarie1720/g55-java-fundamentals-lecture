8220c3d14283cc1eba2e6c6b56b152be99920042
LanaMB <lanamarie1720@gmail.com>
2025-03-04 09:37:08 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/HelloWorld.java
@@ -0,0 +1,20 @@
+package se.lexicon;
+
+public class HelloWorld {
+
+    // Single line comment here
+
+ /*
+Multi-line comment here
+*/
+
+    /**
+     * This is a Java Doc <b>(Documentation comments)</b>.
+     * ## Used to create documentation for **Java Codes**
+     */
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+}
