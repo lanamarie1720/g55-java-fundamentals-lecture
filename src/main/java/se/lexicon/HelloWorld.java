f9e9892eee985ecf11b4c9a5ffb7831d83f23ceb
LanaMB <lanamarie1720@gmail.com>
2025-03-04 10:05:49 +0100

    Updates

--- src/main/java/se/lexicon/HelloWorld.java
+++ src/main/java/se/lexicon/HelloWorld.java
@@ -14,7 +14,26 @@ Multi-line comment here
      */
 
     public static void main(String[] args){
+        // sout + Tab
         System.out.println("Hello World!");
+
+        // Variables
+        // [data type] [variableName];
+        int speed;
+        speed = 100;
+
+        double salary = 50000.55;
+        char grade = 'A';
+        char specialChar = '\u2764'; // '♥'
+        boolean isActive = true;
+        boolean isReserved = false;
+
+        long balance = 50000000000000L;
+        double area = 20.55;
+        int setNumber = 100;
+        int studentId = 5000;
+
+
     }
 
 }
